66b07b6f04b1b054ec0f7b6b223248e4c71623db
melian-e <iemelian.rambeau@student.junia.com>
2023-10-31 11:15:58 +0100

    first menu

--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -1,9 +1,18 @@
 import { FC, ReactElement } from 'react';
+import { Menu } from '@/components/Menu';
+
+const menu = [
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/about' },
+];
 
 const Home: FC = (): ReactElement => (
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Home page
-  </main>
+  <>
+    <Menu menu={menu} />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home page
+    </main>
+  </>
 );
 
 export default Home;
---
+++ library-site/src/components/Menu.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+type MenuProps = {
+  menu: { name: string; link: string }[];
+};
+
+export const Menu: FC<MenuProps> = ({ menu }) => (
+  <div className="w-full bg-slate-600 p-4">
+    {menu.map((item) => (
+      <a
+        className="mr-4 bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+        href={item.link}
+        key={item.name}
+        data-testid={item.name}
+      >
+        {item.name}
+      </a>
+    ))}
+  </div>
+);
