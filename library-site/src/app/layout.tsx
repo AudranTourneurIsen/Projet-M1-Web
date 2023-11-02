8bf53c9fd70503c6eb496c605d6d4d39ad0b908c
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-11-02 16:09:11 +0100

    feat: Graphic redesign

--- library-site/src/app/layout.tsx
+++ library-site/src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} bg-gray-100 dark:bg-gray-900 text-gray-300`}
+      >
         <Menu menu={menu} />
         {children}
       </body>
--- library-site/src/components/Menu.tsx
+++ library-site/src/components/Menu.tsx
@@ -15,11 +15,14 @@ export const Menu: FC<MenuProps> = ({ menu }) => {
   const title = paths.filter((item) => path.match(item.regex))[0].name;
 
   return (
-    <div className="w-full bg-slate-600 grid grid-cols-3 p-1">
+    <div className="w-full flex justify-around items-center p-1 gap-4 sm:gap-8 md:gap-16 lg:gap-24">
+      <span className="hidden lg:block text-3xl">{title}</span>
       <span className="flex">
         {menu.map((item) => (
           <a
-            className="mr-4 bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            className={`mr-4 hover:bg-slate-700 font-bold py-2 px-4 rounded ${
+              title === item.name ? 'text-purple-500' : 'text-gray-100'
+            }`}
             href={item.link}
             key={item.name}
             data-testid={item.name}
@@ -28,12 +31,13 @@ export const Menu: FC<MenuProps> = ({ menu }) => {
           </a>
         ))}
       </span>
-      <span className="text-3xl m-auto">{title}</span>
-      <span className="flex justify-end">
-        <span className="p-2 bg-slate-700 border border-collapse rounded float-right">
-          <Breadcrumbs root="Home" />
+      <div className="justify-end hidden lg:flex">
+        <span className=" relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            <Breadcrumbs root="Home" />
+          </span>
         </span>
-      </span>
+      </div>
     </div>
   );
 };
