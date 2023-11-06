b14f104492e39baf800f8a4160c8c996106d671b
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-11-06 11:40:56 +0100

    authors display

--- library-site/src/app/authors/page.tsx
+++ library-site/src/app/authors/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { FC, useEffect, useState } from 'react';
+import axios from 'axios';
+
 import { useAuthorsProviders } from '@/hooks/providers/authorProviders';
 import { Modal } from '@/components/Modal';
 import { Button } from '@/components/Button';
 import { TextInput } from '@/components/TextInput';
-import axios from 'axios';
 
 const AuthorsPage: FC = () => {
   const { useListAuthors } = useAuthorsProviders();
@@ -21,19 +22,10 @@ const AuthorsPage: FC = () => {
   const [authorLastName, setAuthorLastName] = useState<string>('');
 
   async function submitNewAuthor(): Promise<void> {
-    console.log(authorFirstName, authorLastName);
-
-    try {
-      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/authors/new`, {
-        firstName: authorFirstName,
-        lastName: authorLastName,
-      });
-      console.log(res)
-
-      console.log('réponse serveur =', res);
-    } catch (e) {
-      console.log(e);
-    }
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/authors/new`, {
+      firstName: authorFirstName,
+      lastName: authorLastName,
+    });
   }
 
   return (
@@ -47,6 +39,38 @@ const AuthorsPage: FC = () => {
       >
         Test
       </Button>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Image
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Bibliography
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {authors.map((author) => (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  [TODO]
+                </th>
+                <td className="px-6 py-4">{author.firstName} {author.lastName}</td>
+                <td className="px-6 py-4">[TODO]</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <Modal
         isOpen={isOpen}
@@ -71,7 +95,10 @@ const AuthorsPage: FC = () => {
             value={authorLastName}
           />
           <div className="flex flex-row-reverse">
-            <Button color="success" onPress={(): void => submitNewAuthor()}>
+            <Button
+              color="success"
+              onPress={(): Promise<void> => submitNewAuthor()}
+            >
               Create new author
             </Button>
           </div>
