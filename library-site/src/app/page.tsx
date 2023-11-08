4e6ec6317d185cc763daae3b5490bb494c4e98d9
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-11-08 23:19:12 +0100

    merge front page

---
+++ library-site/src/app/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { FC, ReactElement } from 'react';
+
+const Home: FC = (): ReactElement => (
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="container h-full mx-auto flex justify-center items-center m-4">
+      <div className="space-y-2 text-center">
+        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-purple-400 glitch">
+          The Great Library
+        </h1>
+        <h2 className="dark:text-gray-200 text-xl">
+          A blazingly-fast, no non-sense <br />
+          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-300">
+            library management system
+          </span>{' '}
+          <br /> built for the 21st century{' '}
+        </h2>
+      </div>
+    </div>
+  </main>
+);
+
+export default Home;
