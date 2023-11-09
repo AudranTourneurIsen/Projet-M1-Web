4b00b221b955406c662a61cefd38f46a319f2500
Judith <judith.lecoq@student.junia.com>
2023-11-09 22:10:42 +0100

    Added Badge filter for Books and improvments

--- library-site/src/components/Badge.tsx
+++ library-site/src/components/Badge.tsx
@@ -6,34 +6,29 @@ type BadgeColor = 'unselected' | 'selected';
 
 type BadgeProps = {
     isActive : boolean;
-    color: BadgeColor;
     onPress: () => void;
     label: string;
 }
 export const Badge : FC<BadgeProps> = (props) => {
-    let {isActive = false, color = 'unselected', onPress, label} = props;
+    let {isActive, onPress, label} = props;
+
+    const baseClass = "text-md font-semibold mx-2 px-4 py-2 rounded-xl"
 
     const colorClasses : Record<BadgeColor, string> = {
-        selected: 'bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300',
-        unselected: 'bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300'
+        selected: 'bg-purple-900 text-purple-300',
+        unselected: 'bg-gray-700 text-gray-300'
     };
 
-    function handleClick() {
-        isActive = !isActive;
-        if (color == 'unselected') {
-            color = 'selected';
-        }
-        else {
-            color = 'unselected';
-        }
+    const color = isActive ? 'selected' : 'unselected'
 
+    function handleClick() {
         onPress();
     }
 
     return (
 
           <span
-              className={twMerge(colorClasses[color])}
+              className={twMerge(baseClass, colorClasses[color])}
               onClick={handleClick}
           >
           {label}
