c916dc50c01c51c78aca42c5a12fbd4348485e81
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-11-09 15:23:33 +0100

    front page and background styles

--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -6,10 +6,11 @@ const Home: FC = (): ReactElement => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="container h-full mx-auto flex justify-center items-center m-4">
       <div className="space-y-2 text-center">
-        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-purple-400 glitch">
+        <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-purple-400 glitch">
           The Great Library
         </h1>
-        <h2 className="dark:text-gray-200 text-xl">
+        <div className="h-6"></div>
+        <h2 className="dark:text-gray-200 text-2xl">
           A blazingly-fast, no non-sense <br />
           <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-300">
             library management system
