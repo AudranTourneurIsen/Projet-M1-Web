5da98ae54c25da2847744de5ee2a1f38f8afbdda
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-11-10 12:07:02 +0100

    swagger front

---
+++ library-site/src/app/api/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import { FC } from 'react';
+
+const SwaggerPage: FC = () => (
+  <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+);
+
+export default SwaggerPage;
