6a63a8b308e4fd984f04eeec61d3e1fb0bf2f4d4
melian-e <iemelian.rambeau@student.junia.com>
2023-11-10 09:20:29 +0100

    start of the documentation

--- library-api/src/main.ts
+++ library-api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Library')
+    .setDescription('The Labrary API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.API_PORT ?? 3000;
   await app.listen(port);
